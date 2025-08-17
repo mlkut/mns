ee6d64c49c878b7725572f43183eae20b8d7ca11
nazeh <nuh@nuh.dev>
2025-08-17 20:08:52 +0300

    wip: init RandomX only once

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 use rand::{RngCore, thread_rng};
-use sha2::{Digest, Sha256};
+use randomx_rs::{RandomXCache, RandomXFlag, RandomXVM};
 use std::fmt::Debug;
 
 use ed25519_dalek::SigningKey;
+
+// TODO: benchmark to get appropriate difficulties
 
 /// Controls how fast can a user generate (and examine) a name from the genesis public key.
 ///
@@ -11,7 +13,7 @@ use ed25519_dalek::SigningKey;
 ///
 /// It should be high enough to make it significantly more difficult for an attacker to find
 /// another genesis public key and nonce that hashes to the same Name.
-const NAME_GENERATION_DIFFICULTY: u8 = 16;
+const NAME_GENERATION_DIFFICULTY: u8 = 8;
 /// Controls how fast can someone timestamp newly generated Names and claim them forever.
 ///
 /// Usually a user would generate the name first, and only publish their SingnedPacket once
@@ -19,30 +21,38 @@ const NAME_GENERATION_DIFFICULTY: u8 = 16;
 ///
 /// The longer this takes, the slower initial publishing may take, but the harder it would be
 /// for squatting.
-const TIMESTAMPING_DIFFICULTY: u8 = 24;
+const TIMESTAMPING_DIFFICULTY: u8 = 8;
 
+const RANDOMX_KEY: &str = "mns/randomx/key";
 const NAME_GENERATION_SALT: &str = "mns/name-generation_salt";
 const TIMESTAMPING_SALT: &str = "mns/timestamping_salt";
 
 // Claim Proof: Name Generation Work + Timestamp Work + Timestamp Proof
 //
 // TODO: Adaptive difficulties based on Bitcoin difficulty...
-// Timestamping has to happen within a day/week? of generating the Timestamping work...
+// TODO: Timestamping has to happen within a day/week? of generating the Timestamping work...
+// This disables optimistic proofs... so maybe not!
 
 // TODO: P2P Treap of fresh names pending timestamping... to be timestamped at once ...
 // Basically a p2p version of OTS.
 //
 // TODO: builtin key rotation
 
-pub struct Keypair {
-    hasher: Sha256,
-    signing_key: SigningKey,
-    name_generation_nonce: u64,
-    name_hash: [u8; 32],
+pub struct Mns {
+    vm: RandomXVM,
 }
 
-impl Keypair {
-    pub fn generate() -> Keypair {
+impl Mns {
+    pub fn init() -> Self {
+        // TODO: check that this works the same everywhere...
+        let flags = RandomXFlag::get_recommended_flags();
+        let cache = RandomXCache::new(flags, RANDOMX_KEY.as_bytes()).unwrap();
+        let vm = RandomXVM::new(flags, Some(cache.clone()), None).unwrap();
+
+        Self { vm }
+    }
+
+    pub fn generate(&self) -> Keypair {
         // TODO: use generate_random() instead
         let mut rng = thread_rng();
         let mut seed = [0u8; 32];
@@ -54,10 +64,8 @@ impl Keypair {
 
         let mut name_generation_nonce = 0u64;
 
-        let mut hasher = Sha256::new();
         let name_hash = loop {
-            if let Some(name_hash) =
-                generate_name_hash(&mut hasher, public_key, name_generation_nonce)
+            if let Some(name_hash) = generate_name_hash(&self.vm, public_key, name_generation_nonce)
             {
                 break name_hash;
             }
@@ -66,21 +74,33 @@ impl Keypair {
         };
 
         Keypair {
-            hasher,
             signing_key,
             name_hash,
             name_generation_nonce,
         }
     }
 
-    pub fn genesis(&mut self) -> ([u8; 32], u64, u64) {
-        let mut timestamping_nonce = 0;
+    pub fn verify_name(&self, keypair: &Keypair) -> bool {
+        let expected_name = generate_name_hash(
+            &self.vm,
+            &keypair.public_key(),
+            keypair.name_generation_nonce,
+        );
 
+        if let Some(expected_name) = expected_name {
+            return keypair.name_hash == expected_name;
+        }
+
+        false
+    }
+
+    pub fn genesis(&self, keypair: &Keypair) -> u64 {
+        let mut timestamping_nonce = 0_u64;
         loop {
             let timestamping_work = hash_pow(
-                &mut self.hasher,
+                &self.vm,
                 TIMESTAMPING_SALT,
-                &self.name_hash,
+                &keypair.name_hash,
                 timestamping_nonce,
             );
             if check_pow_target(&timestamping_work, TIMESTAMPING_DIFFICULTY) {
@@ -90,14 +110,29 @@ impl Keypair {
             timestamping_nonce += 1;
         }
 
-        (
-            // TODO: signature over SignedPacket
-            self.signing_key.verifying_key().as_bytes().to_owned(),
-            self.name_generation_nonce,
-            timestamping_nonce,
-        )
+        timestamping_nonce
     }
 
+    pub fn verify_timestamp_pow(&self, keypair: &Keypair, timestamping_nonce: u64) -> bool {
+        check_pow_target(
+            &hash_pow(
+                &self.vm,
+                TIMESTAMPING_SALT,
+                &keypair.public_key(),
+                timestamping_nonce,
+            ),
+            TIMESTAMPING_DIFFICULTY,
+        )
+    }
+}
+
+pub struct Keypair {
+    signing_key: SigningKey,
+    name_generation_nonce: u64,
+    name_hash: [u8; 32],
+}
+
+impl Keypair {
     pub fn name(&self) -> String {
         let name = &self.name_hash[0..5];
 
@@ -108,6 +143,10 @@ impl Keypair {
 
         format!("{}_{}", first_part, second_part)
     }
+
+    pub fn public_key(&self) -> [u8; 32] {
+        self.signing_key.verifying_key().to_bytes()
+    }
 }
 
 impl Debug for Keypair {
@@ -116,25 +155,30 @@ impl Debug for Keypair {
     }
 }
 
-fn generate_name_hash(hasher: &mut Sha256, public_key: &[u8; 32], nonce: u64) -> Option<[u8; 32]> {
-    let name_genreation_work = hash_pow(hasher, NAME_GENERATION_SALT, public_key, nonce);
+fn generate_name_hash(vm: &RandomXVM, public_key: &[u8; 32], nonce: u64) -> Option<[u8; 32]> {
+    let name_genreation_work = hash_pow(vm, NAME_GENERATION_SALT, public_key, nonce);
     if !check_pow_target(&name_genreation_work, NAME_GENERATION_DIFFICULTY) {
         return None;
     };
 
-    hasher.update(NAME_GENERATION_SALT);
-    hasher.update(name_genreation_work);
     // TODO: better type casting.
-    let hash = hasher.finalize_reset()[..].try_into().expect("infallible");
+    let hash = vm
+        .calculate_hash(&name_genreation_work)
+        .unwrap()
+        .try_into()
+        .unwrap();
 
     Some(hash)
 }
 
-fn hash_pow(hasher: &mut Sha256, salt: &str, base: &[u8; 32], nonce: u64) -> [u8; 32] {
-    hasher.update(salt.as_bytes());
-    hasher.update(&base);
-    hasher.update(nonce.to_le_bytes());
-    hasher.finalize_reset()[..].try_into().unwrap()
+fn hash_pow(vm: &RandomXVM, salt: &str, base: &[u8; 32], nonce: u64) -> [u8; 32] {
+    let mut bytes = vec![];
+    bytes.extend_from_slice(salt.as_bytes());
+    bytes.extend_from_slice(base);
+    bytes.extend_from_slice(&nonce.to_be_bytes());
+
+    // TODO: better type casting.
+    vm.calculate_hash(&bytes).unwrap().try_into().unwrap()
 }
 
 fn check_pow_target(hash: &[u8], required_zero_bits: u8) -> bool {
@@ -161,15 +205,32 @@ mod tests {
     #[test]
     fn basic() {
         let start = Instant::now();
-        let mut x = Keypair::generate();
-
-        println!("Generated {x:?} in {} ms", start.elapsed().as_millis());
+        let mns = Mns::init();
+        println!("init {:?}", start.elapsed());
 
         let start = Instant::now();
-        let genesis = x.genesis();
+        let keypair = mns.generate();
+
         println!(
-            "Generated the genesis for {x:?} in {} ms",
+            "Generated {keypair:?} in {} ms",
             start.elapsed().as_millis()
+        );
+
+        let start = Instant::now();
+        assert!(mns.verify_name(&keypair));
+        println!("Validated {keypair:?} name in {:?}...", start.elapsed());
+
+        let start = Instant::now();
+        let timestamping_nonce = mns.genesis(&keypair);
+        println!(
+            "Generated the genesis for {keypair:?} in {} ms",
+            start.elapsed().as_millis()
+        );
+
+        assert!(mns.verify_timestamp_pow(&keypair, timestamping_nonce));
+        println!(
+            "Validated {keypair:?} timestamping pow in {:?}...",
+            start.elapsed()
         );
     }
 }
