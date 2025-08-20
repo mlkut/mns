b5803d6e5386c93c67b665b99089948d77ab0698
nazeh <nuh@nuh.dev>
2025-08-20 19:11:10 +0300

    feat: use signatures inside name generation

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,13 @@ use rayon::iter::{IntoParallelIterator, ParallelIterator};
 use sha2::{Digest, Sha256};
 use std::fmt::Debug;
 
-use ed25519_dalek::{Signer, SigningKey, Verifier};
+use ed25519_dalek::{Signature, Signer, SigningKey, Verifier, VerifyingKey};
+
+mod name;
+
+pub use name::Name;
+
+const DOMAIN_SUFFIX: &str = ".mns.alt";
 
 // TODO: benchmark to get appropriate difficulties
 
@@ -42,113 +48,138 @@ const TIMESTAMPING_TAG: &[u8; 16] = b"mns/timestamping";
 //
 // TODO: builtin key rotation
 
+// TODO: versioning?
 pub struct Claim {
-    owner: Vec<u8>,
-    name_generation_proof: ([u8; 16], u64),
+    public_key: VerifyingKey,
+    name_generation_proof: [u8; 88],
 }
 
 impl Claim {
-    pub fn name(&self) -> String {
-        let name = &self.owner[1..5];
+    /// Generate an initial claim, doing only enough work to examine the generated [name][Claim::name].
+    pub fn generate() -> Self {
+        // TODO: use generate_random() instead
+        let mut rng = thread_rng();
+        let mut seed = [0u8; 32];
+        rng.fill_bytes(&mut seed);
 
-        let input = base32::encode(base32::Alphabet::Z, name);
+        let signing_key = SigningKey::from_bytes(&seed);
 
-        let first_part = &input[..4];
-        let second_part = &input[4..];
+        Claim::generate_from_signing_key(&signing_key)
+    }
 
-        format!("{}_{}", first_part, second_part)
+    pub fn generate_from_signing_key(signing_key: &SigningKey) -> Claim {
+        let public_key = signing_key.verifying_key();
+
+        let name_generation_proof = (0u64..u64::MAX)
+            .into_par_iter()
+            .map(|nonce| {
+                let nonce_bytes = nonce.to_be_bytes();
+                let challenge = hash(&[NAME_GENERATION_TAG, &nonce_bytes]);
+
+                let signature = signing_key.sign(&challenge);
+                let signature_bytes = signature.to_bytes();
+
+                if let Ok(Some(solution)) =
+                    EquiX::new(&challenge).map(|x| x.solve().first().map(|x| x.to_bytes()))
+                {
+                    let pow = hash(&[
+                        NAME_GENERATION_TAG,
+                        &nonce_bytes,
+                        &signature_bytes,
+                        &solution,
+                    ]);
+
+                    if check_pow_target(&pow, NAME_GENERATION_DIFFICULTY) {
+                        let mut result = [0_u8; 88];
+                        result[0..8].copy_from_slice(&nonce_bytes);
+                        result[8..72].copy_from_slice(&signature_bytes);
+                        result[72..].copy_from_slice(&solution);
+
+                        return Some(result);
+                    };
+                };
+
+                None
+            })
+            .find_any(|result| result.is_some())
+            .flatten()
+            .expect("find pow");
+
+        Claim {
+            public_key,
+            name_generation_proof,
+        }
+    }
+
+    pub fn name(&self) -> Name {
+        hash(&[&self.name_generation_proof])[..5]
+            .try_into()
+            .expect("prefix")
+    }
+
+    // pub fn complete(&mut self) {
+    //     (0u64..u64::MAX)
+    //         .into_par_iter()
+    //         .map(|nonce| {
+    //             attempt_pow(
+    //                 TIMESTAMPING_TAG,
+    //                 &keypair.signing_key,
+    //                 TIMESTAMPING_DIFFICULTY,
+    //                 nonce,
+    //             )
+    //             .map(|signature| (signature, nonce))
+    //         })
+    //         .find_any(|result| result.is_some())
+    //         .flatten()
+    // }
+
+    pub fn verify_name(&self) -> bool {
+        let nonce_bytes = &self.name_generation_proof[0..8];
+        let challenge = hash(&[NAME_GENERATION_TAG, &nonce_bytes]);
+
+        let signature_bytes = &self.name_generation_proof[8..72];
+        let solution_bytes = &self.name_generation_proof[72..];
+
+        // Cheaper verification first
+
+        let pow = hash(&[
+            NAME_GENERATION_TAG,
+            &nonce_bytes,
+            &signature_bytes,
+            &solution_bytes,
+        ]);
+
+        if !check_pow_target(&pow, NAME_GENERATION_DIFFICULTY) {
+            return false;
+        };
+
+        if !self
+            .public_key
+            .verify(
+                &challenge,
+                &Signature::from_bytes(signature_bytes.try_into().unwrap()),
+            )
+            .is_ok()
+        {
+            return false;
+        };
+
+        let solution: [u8; 16] = self.name_generation_proof[72..].try_into().unwrap();
+
+        let equix = EquiX::new(&challenge).unwrap();
+
+        equix
+            .verify(&Solution::try_from_bytes(&solution).unwrap())
+            .is_ok()
     }
 }
 
 impl Debug for Claim {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Claim ({}.mns.alt)", self.name())
+        write!(f, "Claim ({}{DOMAIN_SUFFIX})", self.name())
     }
 }
 
-pub fn generate() -> Claim {
-    let owner = owner();
-
-    let name_generation_proof = (0u64..u64::MAX)
-        .into_par_iter()
-        .map(|nonce| {
-            attempt_pow(
-                NAME_GENERATION_TAG,
-                &owner,
-                NAME_GENERATION_DIFFICULTY,
-                nonce,
-            )
-            .map(|solution| (solution, nonce))
-        })
-        .find_any(|result| result.is_some())
-        .flatten()
-        .expect("find pow");
-
-    Claim {
-        owner,
-        name_generation_proof,
-    }
-}
-
-pub fn verify_name(claim: &Claim) -> bool {
-    let (solution, nonce) = claim.name_generation_proof;
-
-    let mut challenge = vec![];
-    challenge.extend_from_slice(NAME_GENERATION_TAG);
-    challenge.extend_from_slice(&claim.owner);
-    challenge.extend_from_slice(&nonce.to_be_bytes());
-
-    let mut hasher = Sha256::new();
-    hasher.update(&challenge);
-    hasher.update(&solution);
-    let pow = &hasher.finalize()[..];
-
-    let start = std::time::Instant::now();
-    dbg!(start.elapsed());
-    let start = std::time::Instant::now();
-
-    if !check_pow_target(&pow, NAME_GENERATION_DIFFICULTY) {
-        return false;
-    };
-
-    return equix::verify_bytes(&challenge, &solution).is_ok();
-
-    dbg!(start.elapsed());
-    let start = std::time::Instant::now();
-
-    dbg!(start.elapsed());
-    let start = std::time::Instant::now();
-    let solution = &Solution::try_from_bytes(&solution).unwrap();
-    dbg!(("solution", start.elapsed()));
-    let start = std::time::Instant::now();
-
-    let x = EquiX::new(&challenge).unwrap();
-    dbg!(("new", start.elapsed()));
-    let start = std::time::Instant::now();
-
-    let x = x.verify(solution).is_ok();
-    dbg!(("verify", start.elapsed()));
-
-    x
-}
-
-// pub fn timestamp_pow(keypair: &Claim) -> ([u8; 64], u64) {
-//     (0u64..u64::MAX)
-//         .into_par_iter()
-//         .map(|nonce| {
-//             attempt_pow(
-//                 TIMESTAMPING_TAG,
-//                 &keypair.signing_key,
-//                 TIMESTAMPING_DIFFICULTY,
-//                 nonce,
-//             )
-//             .map(|signature| (signature, nonce))
-//         })
-//         .find_any(|result| result.is_some())
-//         .flatten()
-//         .expect("find pow")
-// }
-//
 // pub fn verify_timestamp_pow(
 //     keypair: &Claim,
 //     timestamp_signature: &[u8; 64],
@@ -176,27 +207,6 @@ pub fn verify_name(claim: &Claim) -> bool {
 //     Self::verify_name(keypair)
 // }
 
-fn attempt_pow(tag: &[u8], data: &[u8], target: u8, nonce: u64) -> Option<[u8; 16]> {
-    let mut challenge = vec![];
-    challenge.extend_from_slice(tag);
-    challenge.extend_from_slice(data);
-    challenge.extend_from_slice(&nonce.to_be_bytes());
-
-    if let Ok(Some(solution)) =
-        EquiX::new(&challenge).map(|x| x.solve().first().map(|x| x.to_bytes()))
-    {
-        challenge.extend_from_slice(&solution);
-
-        let pow = hash(&challenge);
-
-        if check_pow_target(&pow, target) {
-            return Some(solution);
-        };
-    };
-
-    None
-}
-
 fn check_pow_target(hash: &[u8], required_zero_bits: u8) -> bool {
     let mut zero_bits = 0u8;
 
@@ -212,29 +222,12 @@ fn check_pow_target(hash: &[u8], required_zero_bits: u8) -> bool {
     zero_bits >= required_zero_bits
 }
 
-fn hash(msg: &[u8]) -> [u8; 32] {
+fn hash(msgs: &[&[u8]]) -> [u8; 32] {
     let mut hasher = Sha256::new();
-    hasher.update(msg);
+    for msg in msgs {
+        hasher.update(msg);
+    }
     hasher.finalize().into()
-}
-
-fn owner() -> Vec<u8> {
-    // TODO: use generate_random() instead
-    let mut rng = thread_rng();
-    let mut seed = [0u8; 32];
-    rng.fill_bytes(&mut seed);
-
-    let signing_key = SigningKey::from_bytes(&seed);
-    let public_key = signing_key.verifying_key();
-
-    let signature = signing_key.sign(NAME_GENERATION_TAG);
-
-    let mut owner = vec![];
-    owner.extend_from_slice(public_key.as_bytes());
-    owner.extend_from_slice(signature.r_bytes());
-    owner.extend_from_slice(signature.s_bytes());
-
-    owner
 }
 
 #[cfg(test)]
@@ -246,19 +239,18 @@ mod tests {
     #[test]
     fn basic() {
         let start = Instant::now();
-        println!("init {:?}", start.elapsed());
 
-        let start = Instant::now();
-        let claim = generate();
-
+        println!("Generating a random name..");
+        let claim = Claim::generate();
         println!("Generated {claim:?} in {} ms", start.elapsed().as_millis());
 
         let start = Instant::now();
-        assert!(verify_name(&claim));
+        println!("Verifying the claim on a {}..", claim.name());
+        assert!(claim.verify_name());
         println!("Validated {claim:?} claim in {:?}...", start.elapsed());
 
         // let start = Instant::now();
-        // let (timestamping_signature, timestamping_nonce) = timestamp_pow(&keypair);
+        // let (timestamping_signature, timestamping_nonce) = claim.complete(&keypair);
         // println!(
         //     "Generated the timestamp PoW for {keypair:?} in {} ms",
         //     start.elapsed().as_millis()
